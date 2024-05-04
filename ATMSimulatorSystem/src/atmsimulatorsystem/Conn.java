aca2708cd4cc19043935f71be603e25207a640cd
Nakul Rohilla <108169239+nakulrohilla@users.noreply.github.com>
2024-05-04 15:05:08 +0530

    Add files via upload

---
+++ ATMSimulatorSystem/src/atmsimulatorsystem/Conn.java
@@ -0,0 +1,21 @@
+package atmsimulatorsystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///atmsimulatorsystem","root","badshah490");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+
